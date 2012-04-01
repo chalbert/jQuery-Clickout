a50dc3b60dc89f08c6e23ecbd4de15f6486ef6ea
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-04-02 10:12:40 +1200

    Added jsDoc documentation, Made AMD format optional

--- jquery.clickout.js
+++ jquery.clickout.js
@@ -1,10 +1,51 @@
-define(['jquery'], function($){
+/**
+ * |-----------------|
+ * | jQuery-Clickout |
+ * |-----------------|
+ *  jQuery-Clickout is freely distributable under the MIT license.
+ *
+ *  <a href="https://github.com/chalbert/Backbone-Elements">More details & documentation</a>
+ *
+ * @author Nicolas Gilbert
+ *
+ * @requires jQuery
+ */
 
+(function(factory){
+  'use strict';
+
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery'], factory);
+  } else {
+    factory($);
+  }
+
+})(function ($){
+  'use strict';
+
+     /**
+      * A static counter is tied to the doc element to track click-out registration
+      * @static
+      */
   var counter = 0,
+
+     /**
+      * On mobile Touch browsers, 'click' are not triggered on every element.
+      * Touchstart is.
+      * @static
+      */
       click = window.Touch ? 'touchstart' : 'click';
 
+
+  /**
+   * Shortcut for .on('clickout')
+   *
+   * @param data
+   * @param fn
+   */
+
   $.fn.clickout = function(data, fn) {
-    if (fn == null) {
+    if (!fn) {
       fn = data;
       data = null;
     }
@@ -17,25 +58,57 @@ define(['jquery'], function($){
 
   };
 
-  // Add a special jQuery event
+  /**
+   * Implements the 'special' jQuery event interface
+   * Native way to add non-conventional events
+   */
   jQuery.event.special.clickout = {
+
+    /**
+     * When the event is added
+     * @param handleObj Event handler
+     */
+
     add: function(handleObj){
       counter++;
-      $(this).find(handleObj.selector).attr('data-clickout', counter);
+
+      // Add counter to element
+      var target = handleObj.selector
+          ? $(this).find(handleObj.selector)
+          : $(this);
+      target.attr('data-clickout', counter);
+
+      // When the click is inside, extend the Event object to mark it as so
       $(this).on(click + '.clickout' + counter, handleObj.selector, function(e){
-        e.stopPropagation();
+        e.originalEvent.clickin = $(this).attr('data-clickout');
       });
-      $(document).bind(click + '.clickout' + counter, function(e){
-        if (handleObj.data && handleObj.data.propagate !== true) e.stopPropagation();
-        handleObj.handler.apply(this, arguments);
-      });
+
+      // Bind a click event to the document, to be cought after bubbling
+      $(document).bind(click + '.clickout' + counter, (function(id){
+      // A closure is used create a static id
+        return function(e){
+          // If the click is not inside the element, call the callback
+          if (!e.originalEvent.clickin || e.originalEvent.clickin !== id) {
+            handleObj.handler.apply(this, arguments);
+          }
+        };
+      })(counter));
     },
 
+    /**
+     * When the event is removed
+     * @param handleObj Event handler
+     */
     remove: function(handleObj) {
-      var id = $(this).attr('data-clickout');
+      var target = handleObj.selector
+          ? $(this).find(handleObj.selector)
+          : $(this)
+        , id = target.attr('data-clickout');
+
+      target.removeAttr('data-clickout');
+
       $(document).unbind(click + '.clickout' + id);
       $(this).off(click + '.clickout' + id, handleObj.selector);
-      $(this).find(handleObj.selector).removeAttr('data-clickout');
       return false;
     }
   };
@@ -43,4 +116,3 @@ define(['jquery'], function($){
   return $;
 
 });
-
